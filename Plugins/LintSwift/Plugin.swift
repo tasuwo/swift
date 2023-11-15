4e01c2f371f221fa916a5c2f4e512003ae7164d5
tasuwo <kamuhata.you@gmail.com>
2023-11-16 00:17:54 +0900

    Revert ":fire: 重いのでXcodeProjectPruginをオフにする"
    This reverts commit 3a5822405d629fee730128746ef6bacd69fc496d.

--- Plugins/LintSwift/Plugin.swift
+++ Plugins/LintSwift/Plugin.swift
@@ -1,5 +1,8 @@
 import Foundation
 import PackagePlugin
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+#endif
 
 @main
 struct LintSwiftPlugin {
@@ -27,3 +30,13 @@ extension LintSwiftPlugin: BuildToolPlugin {
         return try createBuildCommands(context: context, targetName: target.name, inputPath: target.directory.string)
     }
 }
+
+#if canImport(XcodeProjectPlugin)
+
+extension LintSwiftPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        return try createBuildCommands(context: context, targetName: target.displayName, inputPath: context.xcodeProject.directory.string)
+    }
+}
+
+#endif
